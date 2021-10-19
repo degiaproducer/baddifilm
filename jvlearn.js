7da5341b6061e885bf2f378cb95695efb22c6843
Di Jia <baddifilm2020@gmail.com>
2021-10-19 20:07:59 +0900

    added title Baddi Film

--- jvlearn.js
+++ jvlearn.js
@@ -1,1 +1,38 @@
-console.log("Hello everyone");
+let person = {
+    firstName: 'Akira',
+    lastName: 'Kurosawa', 
+    age : 30, 
+};
+
+//Dot Notation 
+person.lastName ='Ishiguro';
+
+//Brack Notation 
+let selection ='name';
+person[selection]= 'Kazuo';
+
+console.log (person);
+
+let selectedColors= [ 'red','blue'];
+
+selectedColors[2]= 1;
+console.log[selectedColors];
+console.log(selectedColors.length);
+
+//Perform a task
+
+function greet (firstName, lastName) {
+    console.log ('Hello ' + firstName + ' ' + lastName);
+
+}
+
+greet ('Akira', 'Kurosawa');
+
+//Caculate a value 
+
+function square(number){
+    return number * number;
+}
+
+console.log(square(121));
+
