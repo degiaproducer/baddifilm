257e0296c5dfd3b7c79d8adb9083da67b466cabe
Di Jia <baddifilm2020@gmail.com>
2021-10-20 01:17:07 +0900

    added lightbox for general showreel

--- videopop.js
+++ videopop.js
@@ -1,5 +1,23 @@
-//When the user clicks on div, open the popup 
-function vidClick () {
-    var popup=document.getElementById('myPopup');
-    popup.classList.toggle('show')
-}
+window.document.onkeydown = function(e) {
+    if (!e) {
+      e = event;
+    }
+    if (e.keyCode == 27) {
+      lightbox_close();
+    }
+  }
+  
+  function lightbox_open() {
+    var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+    window.scrollTo(0, 0);
+    document.getElementById('light').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+    lightBoxVideo.play();
+  }
+  
+  function lightbox_close() {
+    var lightBoxVideo = document.getElementById("VisaChipCardVideo");
+    document.getElementById('light').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+    lightBoxVideo.pause();
+  }
