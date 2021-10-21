7d0bca8fa23c08c3c792d55e21ae062d882feb30
Di Jia <baddifilm2020@gmail.com>
2021-10-21 13:04:53 +0900

    added motionGraphic button, added coordinate

---
+++ motionGraphic.js
@@ -0,0 +1,23 @@
+window.document.onkeydown = function(e) {
+    if (!e) {
+      e = event;
+    }
+    if (e.keyCode == 27) {
+      lightbox_close();
+    }
+  }
+  
+  function lightbox_open() {
+    var lightBoxVideo = document.getElementById("motionGraphic");
+    window.scrollTo(0, 1400); 
+    document.getElementById('light').style.display = 'block';
+    document.getElementById('fade').style.display = 'block';
+    lightBoxVideo.pause();
+  }
+  
+  function lightbox_close() {
+    var lightBoxVideo = document.getElementById("motionGraphic");
+    document.getElementById('light').style.display = 'none';
+    document.getElementById('fade').style.display = 'none';
+    lightBoxVideo.pause();
+  }
