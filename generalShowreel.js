480fd5d4d270f337bf0b47647ccb86494175b7b3
Di Jia <baddifilm2020@gmail.com>
2021-10-28 19:09:36 +0900

    changed the height of grid on pc

--- generalShowreel.js
+++ generalShowreel.js
@@ -1,23 +1,23 @@
 window.document.onkeydown = function(e) {
-    if (!e) {
-      e = event;
-    }
-    if (e.keyCode == 27) {
-      generalLightbox_close();
-    }
+  if (!e) {
+    e = event;
   }
-  
-  function generalLightbox_open() {
-    var lightBoxVideo = document.getElementById("generalShowreel");
-    window.scrollTo(0,150);
-    document.getElementById('generalLight').style.display = 'block';
-    document.getElementById('generalFade').style.display = 'block';
-    lightBoxVideo.pause();
+  if (e.keyCode == 27) {
+    generalLightbox_close();
   }
-  
-  function generalLightbox_close() {
-    var lightBoxVideo = document.getElementById("generalShowreel");
-    document.getElementById('generalLight').style.display = 'none';
-    document.getElementById('generalFade').style.display = 'none';
-    lightBoxVideo.pause();
-  }
+}
+
+function motionLightbox_open() {
+  var lightBoxVideo = document.getElementById("generalShowreel");
+
+  document.getElementById('generalLight').style.display = 'block';
+  document.getElementById('generalFade').style.display = 'block';
+  lightBoxVideo.pause();
+}
+
+function motionLightbox_close() {
+  var lightBoxVideo = document.getElementById("generalShowreel");
+  document.getElementById('generalLight').style.display = 'none';
+  document.getElementById('geberalShowreel').style.display = 'none';
+  lightBoxVideo.pause();
+}
